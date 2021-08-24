5e6948ba2f459c1cdd3764c3a401b6c476c459dc
Alexandre Bekor <contato@alexandrebekor.com>
2021-08-24 15:13:04 -0300

    quotation view

--- index.js
+++ index.js
@@ -2,12 +2,32 @@ const express = require('express')
 const app = express()
 const path = require('path')
 
+// Require Lib
+const money = require('./lib/money')
+
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
     res.render('home')
+})
+
+app.get('/quotation', (req, res) => {
+    const { price, amount } = req.query
+    if (price && amount) {
+        const result = money.convert(price, amount)
+        res.render('quotation', {
+            error: false,
+            price: 'U$ ' + money.toMoney(price),
+            amount: 'U$ ' + money.toMoney(amount),
+            result: 'R$ ' + money.toMoney(result)
+        })
+    } else {
+        res.render('quotation', {
+            error: 'Valores Inválidos'
+        })
+    }
 })
 
 app.listen(3000, err => {
