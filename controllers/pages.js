646d53b63fa5686c5d9b8106a66cc254b419868f
Alexandre Bekor <contato@alexandrebekor.com>
2021-12-19 08:46:30 -0300

    added function filter in server to push the data to frontend

--- controllers/pages.js
+++ controllers/pages.js
@@ -1,6 +1,37 @@
 const { getQuotation, getListCurrency } = require('../lib/apiBC')
 const { trade, toCurrency } = require('../lib/convert')
 
+
+const filter = async (req, res) => {
+    const { list } = await getListCurrency()
+    const { filtertoCoinCode } = req.query
+    const listMainFilter = list.filter(item => item[filtertoCoinCode])
+    const filterFinal = listMainFilter[0][filtertoCoinCode].map(item => {
+        const name = item.namein
+        const code = item.codein
+        return {
+            name,
+            code
+        }
+    })
+
+    res.send(filterFinal)
+}
+
+const listFilter = (list, code) => {
+    const listMainFilter = list.filter(item => item[code])
+    const filterFinal = listMainFilter[0][code].map(item => {
+        const name = item.namein
+        const code = item.codein
+        return {
+            name,
+            code
+        }
+    })
+
+    return filterFinal
+}
+
 const home = async (req, res) => {
     const { list } = await getListCurrency()
     const listInitial = list.map(item => {
@@ -15,23 +46,29 @@ const home = async (req, res) => {
         return coin
     })
 
+
     const {
         fromCoinValue,
         toCoinValue,
         fromCoinCode,
-        toCoinCode
+        toCoinCode,
+        filtertoCoinCode
     } = req.query
 
     const fromValue = fromCoinValue ?? 1
     const fromCode = fromCoinCode ?? "BRL"
     const toCode = toCoinCode ?? "USD"
     const selectCoin = listInitial
+    const filterActive = filtertoCoinCode ?? "BRL"
+
+    const teste = listFilter(list, filterActive)
 
     res.render('home', {
         fromCoinValue: fromValue,
         fromCoinCode: fromCode,
         quotation: await getQuotation(fromCode, toCode),
-        selectCoin
+        selectCoin,
+        teste
     })
 }
 
@@ -56,5 +93,6 @@ const quotation = async (req, res) => {
 
 module.exports = {
     home,
-    quotation
+    quotation,
+    filter
 }
--- routes/pages.js
+++ routes/pages.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const controller = require('../controllers/pages')
 
 router.get('/', controller.home)
+router.get('/filter', controller.filter)
 router.get('/quotation', controller.quotation)
 
 module.exports = router
