b6cdc7b24965e1aafacdc125ccccc35b22bab1b2
Alexandre Bekor <contato@alexandrebekor.com>
2021-12-19 13:22:56 -0300

    change the assets path with express.static in index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 // assets
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'public')))
 
 // path to views
 app.set('views', path.join(__dirname, 'views'))
